7b6941828d140a7ad2cb48a2bbff292a08c96c6f
AaronWeber90 <aaronweber@gmx.at>
2022-02-03 17:07:39 +0100

    first push

--- index.js
+++ index.js
@@ -1,141 +1,139 @@
-import {soundData} from "./sounds.js"
-import {quotes} from "./quotes.js"
+import {soundData} from "./sounds.js";
 
-const soundContainer = document.querySelector(".sound-container")
-let audio = ""
-
+const soundContainer = document.querySelector(".sound-container");
+// const favoriteSoundsBtn = document.querySelector(".favorite-sounds-btn");
+const allSoundsBtn = document.querySelector(".all-sounds-btn");
+let audio = "";
 
 // RENDER SOUND BTN
-function renderSoundBtn() { 
-    const allSoundBtn = soundData.map(item => `<button class="${item.isFavorite ? "sound-btn sound-favorite" : "sound-btn"}">${item.name}</button>`).join("")
-    soundContainer.innerHTML = allSoundBtn
+function renderAllSounds() {
+  const allSoundBtn = soundData
+    .map(
+      (item) =>
+        `<button class="${
+          item.isFavorite ? "sound-btn sound-favorite" : "sound-btn"
+        }">${item.name}</button>`
+    )
+    .join("");
+  soundContainer.innerHTML = allSoundBtn;
 }
-renderSoundBtn()
+renderAllSounds();
 
+allSoundsBtn.addEventListener("click", () => {
+  renderAllSounds();
+  addSound();
+});
+
+// RENDER FAVORITE SOUNDS
+// function renderFavoriteSounds() {
+//   const favoriteSounds = soundData
+//     .map((item) => {
+//       return item.isFavorite
+//         ? `<button class="sound-btn sound-favorite">${item.name}</button>`
+//         : null;
+//     })
+//     .join("");
+//   soundContainer.innerHTML = favoriteSounds;
+// }
+// favoriteSoundsBtn.addEventListener("click", () => {
+//   renderFavoriteSounds();
+//   addSound();
+// });
 
 // ADD SOUNDS
 function addSound() {
-    const allBtn = document.getElementsByClassName("sound-btn")
-    for (let i= 0; i < allBtn.length; i++) {
-        allBtn[i].addEventListener("click", function() {
-            playSound(i, allBtn)
-        })
-    }
+  const allBtn = document.getElementsByClassName("sound-btn");
+  for (let i = 0; i < allBtn.length; i++) {
+    allBtn[i].addEventListener("click", function () {
+      playSound(i, allBtn);
+    });
+  }
 }
-addSound()
-
+addSound();
 
 //PLAY SOUND
 function playSound(i, allBtn) {
-    if (audio === "") {
-        audio = new Audio(soundData[i].url)
-        audio.play()
-        allBtn[i].classList.add("active")
-    } else if (!allBtn[i].classList.contains("active")) {
-        audio.pause()
-        audio.currentTime = 0
-        for(let btn of allBtn) {
-            btn.classList.remove("active")
-        }
-        audio = new Audio(soundData[i].url)
-        allBtn[i].classList.add("active")
-        audio.play()
-    } else if (!audio.paused) {
-        audio.pause()
-    } else {
-        audio.play()
+  if (audio === "") {
+    audio = new Audio(soundData[i].url);
+    audio.play();
+    allBtn[i].classList.add("active");
+  } else if (!allBtn[i].classList.contains("active")) {
+    audio.pause();
+    audio.currentTime = 0;
+    for (let btn of allBtn) {
+      btn.classList.remove("active");
     }
+    audio = new Audio(soundData[i].url);
+    allBtn[i].classList.add("active");
+    audio.play();
+  } else if (!audio.paused) {
+    audio.pause();
+  } else {
+    audio.play();
+  }
 
-        volumeInput.addEventListener("change", () => changeVolume(audio))
-        soundEnd(audio, i, allBtn)
+  volumeInput.addEventListener("change", () => changeVolume(audio));
+  soundEnd(audio, i, allBtn);
 }
 
 // SOUND END
 function soundEnd(audioVar, i, allBtn) {
-    audioVar.onended = () => {
-    allBtn[i].classList.remove("active")
-    }
+  audioVar.onended = () => {
+    allBtn[i].classList.remove("active");
+  };
 }
 
-// function showActive(i) {
-//     allBtn[i].classList.add("active")
-// }
-
-// function toggleDisable(disabledVar) {
-//     for (let btn of allBtn) {
-//     btn.disabled = disabledVar
-//     }
-// }
-
-// for (let i= 0; i < allBtn.length; i++) {
-//     allBtn[i].addEventListener("click", function() {
-//         playSound(i)
-//         showActive(i)
-//     })
-// }
-
-const navbtn = document.getElementById("nav-btn")
-const menuEl = document.querySelector(".menu-container")
+const navbtn = document.getElementById("nav-btn");
+const menuEl = document.querySelector(".menu-container");
 navbtn.addEventListener("click", () => {
-    menuEl.classList.toggle("menu-closed")
-    if (menuEl.classList.contains("menu-closed")) {
-        navbtn.innerHTML = `<i class="fas fa-bars"></i>`
-        document.body.style.overflow = "auto"
-    } else {
-        navbtn.innerHTML = `<i class="fas fa-times"></i>`
-        document.body.style.overflow = "hidden"
-    }
-})
+  menuEl.classList.toggle("menu-closed");
+  if (menuEl.classList.contains("menu-closed")) {
+    navbtn.innerHTML = `<i class="fas fa-bars"></i>`;
+    document.body.style.overflow = "auto";
+  } else {
+    navbtn.innerHTML = `<i class="fas fa-times"></i>`;
+    document.body.style.overflow = "hidden";
+  }
+});
 
-const gridSizeInput = document.getElementById("grid-size")
-const gridSizeLabel = document.getElementById("grid-size-label")
+const gridSizeInput = document.getElementById("grid-size");
+const gridSizeLabel = document.getElementById("grid-size-label");
 
 gridSizeInput.addEventListener("change", () => {
-    let gridVar = ""
-    gridSizeLabel.textContent = "Grid Size: " + gridSizeInput.value
-    switch (gridSizeInput.value) {
-        case "1":
-            gridVar = "1"
-            break;
-        case "2":
-            gridVar = "2" 
-            break;
-        case "3":
-            gridVar = "3"
-            break;
-    }   
-    soundContainer.style.gridTemplateColumns = `repeat(${gridVar}, 1fr)` 
-})
+  let gridVar = "";
+  gridSizeLabel.textContent = "Grid Size: " + gridSizeInput.value;
+  switch (gridSizeInput.value) {
+    case "1":
+      gridVar = "1";
+      break;
+    case "2":
+      gridVar = "2";
+      break;
+    case "3":
+      gridVar = "3";
+      break;
+  }
+  soundContainer.style.gridTemplateColumns = `repeat(${gridVar}, 1fr)`;
+});
 
-
-const volumeInput = document.getElementById("audio-volume")
-const volumeLabel = document.getElementById("audio-volume-label")
+const volumeInput = document.getElementById("audio-volume");
+const volumeLabel = document.getElementById("audio-volume-label");
 
 function changeVolume(audioVar) {
-    audioVar.volume = volumeInput.value / 10
-    volumeLabel.textContent = "Volume: " + volumeInput.value
+  audioVar.volume = volumeInput.value / 10;
+  volumeLabel.textContent = "Volume: " + volumeInput.value;
 }
-
-
-function randomQuote() {
-    const quoteContainer = document.querySelector(".quote-container")
-    const rndNum = Math.floor(Math.random() * quotes.length)
-    quoteContainer.innerHTML = ` <p class="quote-text">${quotes[rndNum].quote}</p><p class="quote-owner">- ${quotes[rndNum].owner}</p>`
-}
-randomQuote()
-
 
 // TOGGLE THEME
-const darkThemeBtn = document.querySelector(".dark-theme-btn")
-const lightThemeBtn = document.querySelector(".light-theme-btn")
+const darkThemeBtn = document.querySelector(".dark-theme-btn");
+const lightThemeBtn = document.querySelector(".light-theme-btn");
 
 darkThemeBtn.addEventListener("click", () => {
-    document.documentElement.className = "dark-theme";
-})
+  document.documentElement.className = "dark-theme";
+});
 
 lightThemeBtn.addEventListener("click", () => {
-    document.documentElement.className = "light-theme";
-})
-
+  document.documentElement.className = "light-theme";
+});
 
 //AUDIO SPEED
--- sounds.js
+++ sounds.js
@@ -1,155 +1,34 @@
-export const soundData = [{  
+export const soundData = [
+  {
     id: 1,
-    name:  "Andi fette Scheiße",
-    url: "sounds/andi-fette-scheisse.mp3",
-    isFavorite: false
-},
-{  
+    name: "Crowd Laugh",
+    url: "/sounds/mixkit-crowd-laugh-424.wav",
+    isFavorite: true,
+  },
+  {
     id: 2,
-    name:  "Andi Oaschloch #1",
-    url: "sounds/andi-oaschloch-1.mp3",
-    isFavorite: false
-},
-{  
+    name: "Dog Bark",
+    url: "/sounds/mixkit-dog-barking-twice-1.wav",
+    isFavorite: false,
+  },
+  {
     id: 3,
-    name: "Andi Oaschloch #2",
-    url: "sounds/andi-oaschloch-2.mp3",
-    isFavorite: false
-},
-{  
+    name: "Sweep",
+    url: "/sounds/mixkit-fast-small-sweep-transition-166.wav",
+    isFavorite: true,
+  },
+  {
     id: 4,
-    name: "Beidl in Fut",
-    url: "sounds/beidl-in-fut.mp3",
-    isFavorite: false
-},
-{   id: 5,
-    name: "Büro Bergmann",
-    url: "sounds/buero-bergmann.mp3",
-    isFavorite: false
-},
-{   id: 6,
-    name: "Hirsch AKW",
-    url: "sounds/hirsch-akw.mp3",
-    isFavorite: false
-},
-{  
-    id: 7,
-    name: "Hirsch crash",
-    url: "sounds/hirsch-crash.mp3",
-    isFavorite: false
-},
-{  
-    id: 8,
-    name: "Hubifly Schweiz #1",
-    url: "sounds/hubifly-schweiz-1.mp3",
-    isFavorite: false
-},
-{  
-    id: 9,
-    name: "Hubifly Schweiz #2",
-    url: "sounds/hubifly-schweiz-2.mp3",
-    isFavorite: false
-},
-{  
-    id: 10,
-    name: "Lengi Remix",
-    url: "sounds/lengi-remix.mp3",
-    isFavorite: false
-},
-{  
-    id: 11,
-    name: "Marmeladinger",
-    url: "sounds/marmeladinger.mp3",
-    isFavorite: false
-},
-{  
-    id: 12,
-    name: "Mex findets lustig",
-    url: "sounds/mex-finds-lustig.mp3",
-    isFavorite: false
-},
-{  
-    id: 13,
-    name: "Mex Gürtel gesperrt",
-    url: "sounds/mex-gürtel-gesperrt.mp3",
-    isFavorite: false
-},
-{  
-    id: 14,
-    name: "Mex Lache klick",
-    url: "sounds/mex-lache-klick.mp3",
-    isFavorite: false
-},
-{  
-    id: 15,
-    name: "Mex Lache robot",
-    url: "sounds/mex-lache-robot.mp3",
-    isFavorite: false
-},
-{  
-    id: 16,
-    name: "Mex Lache Sevi",
-    url: "sounds/mex-lache-sevi.mp3",
-    isFavorite: false
-},
-{  
-    id: 17,
-    name: "Mex Lache Behindi",
-    url: "sounds/mex-lacht-behindert.mp3",
-    isFavorite: false
-},
-{  
-    id: 18,
-    name: "Mex beim Psychiater",
-    url: "sounds/mex-remix.mp3",
-    isFavorite: false
-},
-{  
-    id: 19,
-    name: "Mex russian love",
-    url: "sounds/mex-russian-love.mp3",
-    isFavorite: false
-},
-{  
-    id: 20,
-    name: "Mex schaut Fut",
-    url: "sounds/mex-schaut-fut.mp3",
-    isFavorite: false
-},
-{  
-    id: 21,
-    name: "Mex schrei Lache",
-    url: "sounds/mex-schrei-lache.mp3",
-    isFavorite: false
-},
-{  
-    id: 22,
-    name: "Mex Vollgas",
-    url: "sounds/mex-vollgas.mp3",
-    isFavorite: false
-},
-{  
-    id: 23,
-    name: "Mex vs Edi",
-    url: "sounds/mex-vs-edi.mp3",
-    isFavorite: false
-},
-{  
-    id: 24,
-    name: "Ritsch jammert",
-    url: "sounds/ritsch-jammert.mp3",
-    isFavorite: false
-},
-{  
-    id: 25,
-    name: "Stefan Feg Beruhigung",
-    url: "sounds/stefan-feg-beruhigung.mp3",
-    isFavorite: false
-},
-{  
-    id: 26,
-    name: "Stev Arnold",
-    url: "sounds/stev-arnold.mp3",
-    isFavorite: false
-}
-]
+    name: "Trumpet",
+    url: "/sounds/mixkit-trumpet-fanfare-2293.wav",
+    isFavorite: false,
+  },
+  {
+    id: 5,
+    name: "Wind",
+    url: "sounds/mixkit-woosh-wind-1168.wav",
+    isFavorite: false,
+  },
+];
+
+// https://mixkit.co/free-sound-effects/
