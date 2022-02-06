4712952a2cd0436e33db931e00abfea47c0318f8
AaronWeber90 <aaronweber@gmx.at>
2022-02-06 22:49:58 +0100

    fix sound file paths

--- sounds.js
+++ sounds.js
@@ -2,31 +2,31 @@ export const soundData = [
   {
     id: 1,
     name: "Crowd Laugh",
-    url: "/sounds/mixkit-crowd-laugh-424.wav",
+    url: "./sounds/mixkit-crowd-laugh-424.wav",
     isFavorite: false,
   },
   {
     id: 2,
     name: "Dog Bark",
-    url: "/sounds/mixkit-dog-barking-twice-1.wav",
+    url: "./sounds/mixkit-dog-barking-twice-1.wav",
     isFavorite: false,
   },
   {
     id: 3,
     name: "Sweep",
-    url: "/sounds/mixkit-fast-small-sweep-transition-166.wav",
+    url: "./sounds/mixkit-fast-small-sweep-transition-166.wav",
     isFavorite: false,
   },
   {
     id: 4,
     name: "Trumpet",
-    url: "/sounds/mixkit-trumpet-fanfare-2293.wav",
+    url: "./sounds/mixkit-trumpet-fanfare-2293.wav",
     isFavorite: false,
   },
   {
     id: 5,
     name: "Wind",
-    url: "sounds/mixkit-woosh-wind-1168.wav",
+    url: "./sounds/mixkit-woosh-wind-1168.wav",
     isFavorite: false,
   },
 ];
